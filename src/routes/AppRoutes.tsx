9c5d868acfbe98aed4c27e7c82944c3b65e9d54a
Hector Joel <joel.c.98@hotmail.com>
2026-07-07 20:47:42 -0300

    first commit

---
+++ src/routes/AppRoutes.tsx
@@ -0,0 +1,12 @@
+import { lazy } from "react";
+import { Route, Routes } from "react-router";
+
+const GamePage = lazy(() => import("../pages/GamePage"));
+
+export const AppRoutes = () => {
+    return (
+    <Routes>
+        <Route path="/"  Component={GamePage}/>
+    </Routes>
+    );
+};
