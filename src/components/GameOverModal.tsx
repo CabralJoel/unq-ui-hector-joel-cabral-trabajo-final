d3e5a991e6555118e1ee24a9af1701b9d0023559
Hector Joel <joel.c.98@hotmail.com>
2026-07-10 19:57:38 -0300

    refactor modal input

--- src/components/GameOverModal.tsx
+++ src/components/GameOverModal.tsx
@@ -1,34 +1,58 @@
+import { useState } from "react";
 import { Button } from "./ui/Button";
+import { Input } from "./ui/Input";
 
 type GameOverModalProps = React.ComponentProps<"div"> & {
     words: string[];
-    onClose: () => void;
+    onClose: (name:string) => void;
 };
 
 export const GameOverModal = ({words,onClose}:GameOverModalProps)=>{
-
+    const [name, setName] = useState("");
 
     const score = words.reduce((total, word) => total + word.length,0,);
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement, HTMLInputElement>,) => {
+        const value = e.target.value;
+
+        setName(value.toUpperCase());
+    };
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        onClose(name);
+    };
     
     return(
         <div className="flex items-center justify-center z-10 fixed inset-0 bg-black/20 ">
             <div className="flex flex-col items-center max-w-2xl rounded-lg border-2 p-10 gap-8 bg-modal shadow-2xl">
+
                 <h2 className="text-2xl">¡Partida terminada!</h2>
-                <span>Puntaje</span>
-                <span className="text-lg">{score}</span>
-                <div className="flex flex-col gap-8 items-center">
-                    <p className="text-lg">Palabras usadas: {words.length}</p>
-                    <p className="wrap-break-word">
-                            {words.length === 0
-                                ? "-"
-                                : words.join(" → ")}
-                        </p>
-                </div>
-                <Button onClick={onClose}>
-                        Nueva Partida
-                </Button>
+
+                <span className="text-lg">Puntaje {score}</span>
+
+                <form className="flex flex-col items-center gap-8"onSubmit={handleSubmit}>
+                    <Input className="p-2 text-center uppercase"
+                    value={name} placeholder="AAA" maxLength={3} onChange={handleInputChange}/>
+
+                    <div className="flex flex-col gap-8 items-center">
+
+                        <span className="text-lg">Palabras usadas: {words.length}</span>
+
+                        <p className="wrap-break-word">
+                                {words.length === 0
+                                    ? "-"
+                                    : words.join(" → ")}
+                            </p>
+                    </div>
+
+                    <Button disabled={name.length !== 3} type="submit">
+                            Guardar y jugar otra vez
+                    </Button>
+                </form>
+                
             </div>
-            
         </div>
     )
 }
